bc021b40c02f00774e7eaf4dd938cf03035d0b0a
Timur Gain <timur.gain@gmail.com>
2023-03-27 23:23:16 +0300

    feat: Express app init, ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:node/recommended'],
+  rules: {
+    // add any custom rules here
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'node/no-unpublished-require': [
+      'error',
+      {
+        allowModules: ['electron'],
+      },
+    ],
+  },
+  env: {
+    node: true,
+    // add any other environments here if necessary
+  },
+  settings: {
+    'import/core-modules': ['express'],
+  },
+};
