f08799dd8b818cd1aa1766bf38817f4342936123
Timur Gain <timur.gain@gmail.com>
2023-04-01 15:53:15 +0300

    feat: patchUserMeAvatar controller

--- controllers/users.js
+++ controllers/users.js
@@ -34,15 +34,34 @@ function getUserById(req, res) {
 function postUser(req, res) {
   const { name, about, avatar } = req.body;
   UserModel.create({ name, about, avatar })
-    .then((queryObj) => res.status(constants.HTTP_STATUS_CREATED).send(queryObj))
+    .then((queryObj) =>
+      res.status(constants.HTTP_STATUS_CREATED).send(queryObj)
+    )
     .catch((err) => handleError(res, err));
 }
 
 function patchUserMe(req, res) {
-  UserModel.findByIdAndUpdate({ _id: req.user._id }, req.body, {
-    returnDocument: 'after',
-    runValidators: true,
-  })
+  UserModel.findByIdAndUpdate(
+    { _id: req.user._id },
+    { name: req.body.name, about: req.body.about },
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    }
+  )
+    .then((queryObj) => res.send(queryObj))
+    .catch((err) => handleError(res, err));
+}
+
+function patchUserMeAvatar(req, res) {
+  UserModel.findByIdAndUpdate(
+    { _id: req.user._id },
+    { avatar: req.body.avatar },
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    }
+  )
     .then((queryObj) => res.send(queryObj))
     .catch((err) => handleError(res, err));
 }
@@ -52,4 +71,5 @@ module.exports = {
   getUserById,
   postUser,
   patchUserMe,
+  patchUserMeAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const jsonParser = require('express').json();
 const {
-  getUsers, getUserById, postUser, patchUserMe,
+  getUsers, getUserById, postUser, patchUserMe, patchUserMeAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
 router.post('/', jsonParser, postUser);
 router.patch('/me', jsonParser, patchUserMe);
-router.patch('/me/avatar', jsonParser, patchUserMe);
+router.patch('/me/avatar', jsonParser, patchUserMeAvatar);
 
 module.exports = router;
